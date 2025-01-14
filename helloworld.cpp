08a2fd8a0e0431b9532adeeaf296b7760b563d6d
rishu33 <rishu35sharma@gmail.com>
2025-01-14 21:31:24 +0000

    add_first_file

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+    // This is a single-line comment.
+    
+    std::cout << "I like pizza!";                // Standard character output
+    std::cout << std::endl;                      // End of line
+    std::cout << "It's really good!" << '\n';    // '\n' is another way to line-break
+    std::cout << "Do you like pizza?";
+    
+    /*
+    This
+    is
+    a
+    multi-line
+    comment.
+    */
+    
+    return 0;
+}
