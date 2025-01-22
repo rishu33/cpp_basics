e357ef4e131cce8937e057cdae8c539debe7f8be
rishu33 <rishu35sharma@gmail.com>
2025-01-22 22:27:50 +0000

    add_for_loop

---
+++ 27_for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    
+    int number;
+
+    for(int i=10 ; i >= 1 ; i--){
+        std::cout << i << '\n';
+    }
+
+    std::cout << "HAPPY NEW YEAR!\n";
+    
+    return 0;
+}
