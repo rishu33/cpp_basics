f05f2568f8d962ad2cdea186ac7d1a5cba54fd27
rishu33 <rishu35sharma@gmail.com>
2025-03-17 21:29:21 +0000

    update_fill_function

--- 50_Fill_function_user_input.cpp
+++ 50_Fill_function_user_input.cpp
@@ -3,17 +3,25 @@
 int main(){
 
     std::string foods[5];
+    std::string temp;
     int size = sizeof(foods)/sizeof(foods[0]);
 
     for(int i = 0; i < size; i++){
-        std::cout << "Enter #" << i+1 << " food you like: ";
-        std::getline(std::cin, foods[i]);
+        std::cout << "Enter #" << i+1 << " food you like or 'q' to quit: ";
+        std::getline(std::cin, temp);
+
+        if(temp == "q"){
+            break;
+        }
+        else{
+            foods[i] = temp;
+        }
     }
     
     std::cout << "You like the following foods:\n";
 
-    for(std::string food:foods){
-        std::cout << food << ' ';
+    for(int i = 0; !foods[i].empty(); i++){
+        std::cout << foods[i] << ' ';
     }
     std::cout << '\n';
 
