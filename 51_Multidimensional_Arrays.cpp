f83aa1384d84e0163fd94f1cae4f2c35feac6867
rishu33 <rishu35sharma@gmail.com>
2025-03-18 22:59:42 +0000

    update_multiD_arrays

--- 51_Multidimensional_Arrays.cpp
+++ 51_Multidimensional_Arrays.cpp
@@ -2,28 +2,11 @@
 
 int main(){
 
-    std::string foods[5];
-    std::string temp;
-    int size = sizeof(foods)/sizeof(foods[0]);
+    std::string cars[][3] = {{"Mustang", "Escape", "F-150"},
+                            {"Corvett", "Equinox", "Silverado"},
+                            {"Challenger", "Durango", "Ram 1500"}};
 
-    for(int i = 0; i < size; i++){
-        std::cout << "Enter #" << i+1 << " food you like or 'q' to quit: ";
-        std::getline(std::cin, temp);
-
-        if(temp == "q"){
-            break;
-        }
-        else{
-            foods[i] = temp;
-        }
-    }
-    
-    std::cout << "You like the following foods:\n";
-
-    for(int i = 0; !foods[i].empty(); i++){
-        std::cout << foods[i] << ' ';
-    }
-    std::cout << '\n';
+    std::cout  << cars[0][0] << '\n';
 
     return 0;
 
