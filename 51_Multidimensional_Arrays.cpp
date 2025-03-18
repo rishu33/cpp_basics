d533f2b1c905976650066715b4d9c00305ae408d
rishu33 <rishu35sharma@gmail.com>
2025-03-18 23:01:45 +0000

    update_multiD_arrays

--- 51_Multidimensional_Arrays.cpp
+++ 51_Multidimensional_Arrays.cpp
@@ -6,7 +6,17 @@ int main(){
                             {"Corvett", "Equinox", "Silverado"},
                             {"Challenger", "Durango", "Ram 1500"}};
 
-    std::cout  << cars[0][0] << '\n';
+    std::cout  << cars[0][0] << '  ';
+    std::cout  << cars[0][1] << '  ';
+    std::cout  << cars[0][2] << '\n';
+
+    std::cout  << cars[1][0] << '  ';
+    std::cout  << cars[1][1] << '  ';
+    std::cout  << cars[1][2] << '\n';
+
+    std::cout  << cars[2][0] << '  ';
+    std::cout  << cars[2][1] << '  ';
+    std::cout  << cars[2][2] << '\n';
 
     return 0;
 
